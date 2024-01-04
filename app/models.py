448a699dc9d73e7f5621ebd92ee842a367c50296
radiantbrimstonemain <melxth0e@gmail.com>
2024-01-04 14:25:36 -0600

    almost

--- app/models.py
+++ app/models.py
@@ -1,7 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from flask_login import UserMixin
-from werkzeug.security import generate_password_hash
 
 db = SQLAlchemy()
 
@@ -10,8 +9,17 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String, unique=True, nullable=False)
     spotify_id = db.Column(db.String, unique=True, nullable=True)
     created_on = db.Column(db.DateTime, default=datetime.utcnow())
+    songs = db.relationship('Song', backref='user', lazy=True)
 
 
     def __init__(self, username, spotify_id=None):
         self.username = username
         self.spotify_id = spotify_id
+
+
+class Song(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,9 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-from flask import render_template, redirect, url_for, flash, request
+from flask import render_template, redirect, url_for, flash, request, send_file, session
 from flask_login import login_user, current_user, login_required, logout_user
 from app import app
-from app.models import User, db
+from app.models import User, db, Song
 
 
 
@@ -22,10 +22,13 @@ def home():
 
 
 
+
 @app.route('/spotify_login')
 def spotify_login():
     auth_url = sp_oauth.get_authorize_url()
     return redirect(auth_url)
+
+
 
 
 
@@ -58,19 +61,73 @@ def spotify_callback():
 def logout():
     flash('Successfully logged out!', 'warning')
     logout_user()
+    session.clear()
     return redirect(url_for('home'))
     
 
 
-@app.route('/discover')
-def discover():
-    return render_template('discover.html')
 
+@app.route('/discover', methods=['GET', 'POST'])
+@login_required
+def discover():
+    static_song_list = ['Song 1', 'Song 2', 'Song 3', 'Song 4']
+
+    spotify_song_list = []
+    if request.method == 'POST':
+        song_search = request.form['song_search']
+        rating = int(request.form['rating'])
+        description = request.form['description']
+
+        new_song = Song(title=song_search, rating=rating, description=description, user_id=current_user.id)
+
+        current_user.songs.append(new_song)
+
+        db.session.add(new_song)
+        db.session.commit()
+
+        return redirect(url_for('discover'))
+
+    return render_template('discover.html', static_song_list=static_song_list, spotify_song_list=spotify_song_list )
+
+
+
+
+
+
+
+def get_recently_played_tracks():
+    token_info = sp_oauth.get_cached_token()
+
+    access_token = token_info['access_token']
+    sp = spotipy.Spotify(auth=access_token)
+
+    time_period = 'pasthour'
+
+    time_range_map = {
+        'pasthour': 'short_term',
+    }
+
+    range_param = time_range_map.get(time_period, 'short_term')
+    try:
+        results = sp.current_user_recently_played(limit=25, time_range=range_param)
+        recently_played_tracks = results['items']
+    except Exception as e:
+        print(f"Error fetching recently played tracks: {e}")
+        recently_played_tracks = []
+
+    return recently_played_tracks
 
 
 @app.route('/recently_played')
+@login_required
 def recently_played():
-    return render_template('recently-played.html')
+    if not current_user.is_authenticated:
+        return redirect(url_for('spotify_login'))
+    
+    recently_played_tracks = get_recently_played_tracks()
+    print(recently_played_tracks)
+    return render_template('recently-played.html', recently_played_tracks=recently_played_tracks)
+
 
 
 
@@ -81,21 +138,35 @@ def lyrics():
 
 
 
+
+
+
 @app.route('/topartists')
 @login_required
 def topArtists():
     if not current_user.is_authenticated:
         return redirect(url_for('spotify_login'))
 
+    time_period = request.args.get('time_period', 'short_term')
 
-    token_info = sp_oauth.get_access_token(user=current_user)
+    token_info = sp_oauth.get_cached_token()
+    
+    if not token_info:
+        return redirect(url_for('spotify_login'))
+
     access_token = token_info['access_token']
 
-    
     sp = spotipy.Spotify(auth=access_token)
 
-    
-    top_artists = sp.current_user_top_artists(limit=15)
+    time_range_map = {
+        'past2weeks': 'short_term',
+        'past6months': 'medium_term',
+        'lifetime': 'long_term',
+    }
+
+    range_param = time_range_map.get(time_period, 'short_term')
+
+    top_artists = sp.current_user_top_artists(limit=15, time_range=range_param)
 
     artists_info = []
     for artist in top_artists['items']:
@@ -105,8 +176,7 @@ def topArtists():
             'popularity': artist['popularity'],
         })
 
-    return render_template('artists.html', top_artists=artists_info)
-
+    return render_template('artists.html', top_artists=artists_info, time_period=time_period)
 
 
 
@@ -115,11 +185,80 @@ def topArtists():
 
 
 @app.route('/topgenres')
+@login_required
 def topGenres():
-    return render_template('genres.html')
+    if not current_user.is_authenticated:
+        return redirect(url_for('spotify_login'))
+
+    time_period = request.args.get('time_period', 'short_term')
+
+    token_info = sp_oauth.get_cached_token()
+
+    if not token_info:
+        return redirect(url_for('spotify_login'))
+
+    access_token = token_info['access_token']
+    sp = spotipy.Spotify(auth=access_token)
+
+    time_range_map = {
+        'past2weeks': 'short_term',
+        'past6months': 'medium_term',
+        'lifetime': 'long_term'
+    }
+
+    range_parameter = time_range_map.get(time_period, 'short_term')
+
+    top_artists = sp.current_user_top_artists(limit=15, time_range=range_parameter)
+
+    top_genres = set()  # Use a set to eliminate duplicates
+
+    for artist in top_artists['items']:
+        genres = artist.get('genres', [])
+        top_genres.update(genres)
+
+    genre_info = [{'name': genre} for genre in top_genres]
+
+    return render_template('genres.html', top_genres=genre_info, time_period=time_period)
+
+
 
 
 
 @app.route('/toptracks')
+@login_required
 def topTracks():
-    return render_template('songs.html')
+    if not current_user.is_authenticated:
+        return redirect(url_for('spotify_login'))
+    
+    time_period = request.args.get('time_period', 'short_term')
+
+    token_info = sp_oauth.get_cached_token()
+
+    if not token_info:
+        return redirect(url_for('spotify_login'))
+    
+    access_token = token_info['access_token']
+
+    sp = spotipy.Spotify(auth=access_token)
+
+    time_range_map = {
+        'past2weeks': 'short_term',
+        'past6months': 'medium_term',
+        'lifetime': 'long_term',
+    }
+
+    range_param = time_range_map.get(time_period, 'short_term')
+
+    top_tracks = sp.current_user_top_tracks(limit=15, time_range=range_param)
+
+    track_info = []
+    for track in top_tracks['items']:
+        track_info.append({
+            'name': track['name'],
+            'image_url': track['album']['images'][0]['url'] if track['album']['images'] else None,
+        })
+
+
+    return render_template('songs.html', top_tracks=track_info, time_period=time_period)
+
+
